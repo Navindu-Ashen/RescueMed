9ffdc0cf52df98004bfa6ec92501005224930e27
Navindu Ashen <navindu158@gmail.com>
2024-06-22 10:03:36 +0530

    Creating projects for all user groups

--- rescue_med/android/app/src/main/kotlin/com/example/rescue_med/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.rescue_med
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ rescue_med_hospital/android/app/src/main/kotlin/com/example/rescue_med_hospital/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rescue_med_hospital
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
